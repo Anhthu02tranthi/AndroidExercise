2ef9a060eb783d6c77e52742dab7054136abb0e4
Anhthu02tranthi <thuttade160388@fpt.edu.vn>
2024-06-14 16:07:59 +0700

    Exercise4_1

---
+++ Exercise4_1/app/src/main/java/com/example/exercise4_1/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.exercise4_1;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.exercise4_1.R;
+
+
+public class MainActivity1 extends AppCompatActivity {
+    public Button myButton1;
+    public EditText myUser1;
+    public EditText myPass1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),
+                (v, insets) -> {
+                    Insets systemBars =
+                            insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top,
+                            systemBars.right, systemBars.bottom);
+                    return insets;
+                });
+        //Views
+        myButton1 = findViewById(R.id.btnOk);
+        myUser1 = findViewById(R.id.editUser);
+        myPass1 = findViewById(R.id.editPassword);
+        //Handling event
+        myButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (myUser1.getText().toString().equals("admin") &&
+                        myPass1.getText().toString().equals("12345"))
+                {
+
+                    Toast.makeText(getApplicationContext(),"Login successful",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    Toast.makeText(getApplicationContext(),"Login failed ",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
+
